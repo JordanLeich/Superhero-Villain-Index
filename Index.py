ea2fc7a96b4a40367c628852009f13f40b7e01e8
Joban Kang <jkang109@my.bcit.ca>
2021-09-12 02:09:57 -0700

    added basic functionality

--- Index.py
+++ Index.py
@@ -13,35 +13,48 @@ class Hero:
         self.powers = powers
         self.power_ranking = power_ranking
 
-        def details():
-            Superman = Hero('Clark Kent, Kal-El', 'Immortal age/mid 20 or 30s', 'alive', 'DC', 'Justice League',
-                            'super strength, flight, invulnerability, super speed, heat vision, freeze breath, x-ray vision, superhuman hearing, healing factor',
-                            448000)
-            Batman = Hero('Bruce Wayne', 48, 'alive', 'DC', 'Justice League', 'no superpowers', 145)
-            Thor = Hero('Donald Blake, Sigurd Jarlson, Jake Olsen, Donar the Mighty', 1500, 'alive', 'Marvel',
-                        'Avengers',
-                        'superhuman strength, speed, agility, durability and immunity to most diseases', 493000)
-            Loki = Hero('Loki Laufeyson, Loki Odinson', 1000, 'deceased', 'Marvel', 'none',
-                        'strength, durability, and longevity far superior to humans', 616)
-            Hulk = Hero('Bruce Banner', 49, 'alive', 'Marvel', 'Avengers', 'strength ', 6900)
-            Daredevil = Hero('Matt Murdock', 25, 'alive', 'Marvel', 'none',
-                             'Superhuman senses Echolocative radar sense Skilled acrobat, martial artist, and stick fighter Utilization of specially-designed billy club',
-                             83)
-            Deadpool = Hero('Wade Wilson', 'immortal, over 800', 'alive', 'Marvel', 'none',
-                            'accelerated healing factor',
-                            115)
-            Wolverine = Hero('Logan', 'over 200', 'alive', 'Marvel', 'X-Men',
-                             'superhuman strength and reflexes, enhanced senses and tracking abilities, and a special healing power that also slows his aging.',
-                             79)
-            Captain_America = Hero('Steve Rodgers', 99, 'unknown', 'Marvel', 'Avengers',
-                                   'Peak-Human Condition, Accelerated Healing Factor, Enhanced Intelligence, Longevity',
-                                   94)
-            # TODO add more heroes/villains
+    def print_hero(self):
+        print('Superhero Name: ' + self.name + '\n'
+              'Age: ' + str(self.age) + '\n'
+              'Status: ' + self.status + '\n'
+              'Creator: ' + self.creator + '\n'
+              'Team: ' + self.team + '\n'
+              'Powers: ' + self.powers + '\n'
+              'Power Ranking: ' + str(self.power_ranking))
+
+def create_heroes():
+    Superman = Hero('Superman', 'Immortal age/mid 20 or 30s', 'alive', 'DC', 'Justice League',
+                    'super strength, flight, invulnerability, super speed, heat vision, freeze breath, x-ray vision, superhuman hearing, healing factor',
+                    448000)
+    Batman = Hero('Batman', 48, 'alive', 'DC', 'Justice League', 'no superpowers', 145)
+    Thor = Hero('Thor', 1500, 'alive', 'Marvel',
+                'Avengers',
+                'superhuman strength, speed, agility, durability and immunity to most diseases', 493000)
+    Loki = Hero('Loki', 1000, 'deceased', 'Marvel', 'none',
+                'strength, durability, and longevity far superior to humans', 616)
+    Hulk = Hero('Hulk', 49, 'alive', 'Marvel', 'Avengers', 'strength ', 6900)
+    Daredevil = Hero('Daredevil', 25, 'alive', 'Marvel', 'none',
+                     'Superhuman senses Echolocative radar sense Skilled acrobat, martial artist, and stick fighter Utilization of specially-designed billy club',
+                     83)
+    Deadpool = Hero('Deadpool', 'immortal, over 800', 'alive', 'Marvel', 'none',
+                    'accelerated healing factor',
+                    115)
+    Wolverine = Hero('Wolverine', 'over 200', 'alive', 'Marvel', 'X-Men',
+                     'superhuman strength and reflexes, enhanced senses and tracking abilities, and a special healing power that also slows his aging.',
+                     79)
+    Captain_America = Hero('Captain America', 99, 'unknown', 'Marvel', 'Avengers',
+                           'Peak-Human Condition, Accelerated Healing Factor, Enhanced Intelligence, Longevity',
+                           94)
+    heroes = [Superman, Batman, Thor, Loki, Hulk, Daredevil, Deadpool, Wolverine, Captain_America]
+    return heroes
 
 
 def start():
     choice = str(input('Enter a superheros name: '))
-    print()
+    heroes = create_heroes()
+    for i in heroes:
+        if i.name == choice:
+            Hero.print_hero(i)
 
     # TODO once a superhero name is inputted, print out all the details about that hero.
 
