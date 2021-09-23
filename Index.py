68f21be86d41523c82b9743095e3deb7318a69b5
Jordan Leich <jordanleich@gmail.com>
2021-09-23 15:04:35 -0400

    Small reformats and fixed typos

--- Index.py
+++ Index.py
@@ -11,9 +11,10 @@ def find_characters(character):
     if character.lower() in all_characters:
         return all_characters[character]
     else:
-        return None # character not found
+        return None  # character not found
 
-# returns character if found or None if not found 
+
+# returns character if found or None if not found
 # handles if character not found and would like to add character to text doc
 def find_character(character):
     characters = find_characters(character)
@@ -26,28 +27,31 @@ def find_character(character):
         if write_to_text_choice.lower() in ['y', 'yes', 'sure']:
             request_a_character(character)
         elif write_to_text_choice.lower() not in ['n', 'no', 'nope']:
-            error_message() # TODO reprompt user to fix mistake made
+            error_message()  # TODO prompt user to fix mistake made
     return characters
+
 
 def character_total(character):  # Gets total value from all of the characters traits
     return character.durability + character.energy + character.fighting + character.intelligence + character.speed + character.strength
+
 
 # characters battle it out
 # generic function serving one_vs_one and two_vs_two
 def character_fight(first_character, first_name, second_character, second_name, stat_not_equal):
     # bonuses randomly distributed to give the character with less fighting stats a chance
-    char_one_total = first_character + randint(0, 6)  
+    char_one_total = first_character + randint(0, 6)
     if stat_not_equal:
         char_one_total += randint(0, 6)
     # character with lower fighting stat only gets 1 bonus but at least it cannot be a 0
     # stat_not_equal will evaluate to 1 when True, 0 when False
-    char_two_total = second_character + randint(stat_not_equal, 6) 
+    char_two_total = second_character + randint(stat_not_equal, 6)
     if char_one_total > char_two_total:
         return first_name
     elif char_one_total < char_two_total:
         return second_name
     else:
         return "It's a draw!"
+
 
 def one_vs_one(first_character, second_character):
     # Checks which fighter has a higher fighting stat to get a slight bonus
@@ -61,6 +65,7 @@ def one_vs_one(first_character, second_character):
     elif first_character.fighting == second_character.fighting:
         return character_fight(character_one, one_name, character_two, two_name, False)
 
+
 def two_vs_two(team1char1, team1char2, team2char1, team2char2):
     # grab stats for each of the teams
     team1power = character_total(team1char1) + character_total(team1char2)
@@ -73,17 +78,18 @@ def two_vs_two(team1char1, team1char2, team2char1, team2char2):
     elif team1power == team2power:
         return character_fight(team1power, 'Team 1', team2power, 'Team 2', False)
 
+
 def free_for_all(ffa_characters):
-    # TODO Fix ffa_characters not returning the correct winner in a ffa battle.
     # sourcery skip: inline-immediately-returned-variable
     # Perform free for all battle
     # Pick a random 2 characters to fight in the list
     # Remove the looser in the list of active characters
     # Pick a random character in the list who hasn't fought yet
     # Evaluate for the winner and repeat until there is only 1 character left
-
+    # TODO Fix ffa_characters not returning the correct winner in a ffa battle.
     winner = 'winner'
     return winner
+
 
 # recursive until character properly selected
 def prompt_character_selection(input_text):
@@ -94,7 +100,8 @@ def prompt_character_selection(input_text):
         return prompt_character_selection(input_text)
     return first
 
-# recursive if improper input selected in menu to reprompt user
+
+# recursive if improper input selected in menu to prompt user
 def versus():
     # Prompt for battle mode:
     mode = input('(1) 1v1\n'
@@ -148,14 +155,14 @@ def versus():
         print(colors.green + 'The winner is :' + winner, colors.reset, '\n')
     elif mode == '4':
         print('Returning the Main Menu\n')
-    else: # any choice besides those listed
+    else:  # any choice besides those listed
         print('Invalid choice, please choose an option\n')
         versus()
     time.sleep(2)
 
 
 def request_a_character(choice):
-    with open("characters_to_add.txt", "a") as f: # file closes when outside this code block
+    with open("characters_to_add.txt", "a") as f:  # file closes when outside this code block
         f.write(str(choice) + '\n')
     print(colors.green + 'Name has been added to the list! You can now go to '
                          'https://github.com/JordanLeich/Superhero-Villain-Index, fork the project, create '
@@ -163,14 +170,15 @@ def request_a_character(choice):
                          'the developers.\n', colors.reset)
     time.sleep(3)
 
-# will be recursive when an error - so to reprompt user with menu and fix their error
+
+# will be recursive when an error - so to prompt user with menu and fix their error
 def extras_menu():
     choice = input('(1) View project releases/newest changes\n'
-                    '(2) Credits\n'
-                    '(3) Request a hero/villain to be added\n'
-                    '(4) Return to main menu\n'
-                    '(5) Exit Program\n\n'
-                    'Which option would you like to pick: ')
+                   '(2) Credits\n'
+                   '(3) Request a hero/villain to be added\n'
+                   '(4) Return to main menu\n'
+                   '(5) Exit Program\n\n'
+                   'Which option would you like to pick: ')
     print()
     if choice == '1':
         webbrowser.open('https://github.com/JordanLeich/Superhero-Index/releases')
@@ -188,7 +196,7 @@ def extras_menu():
             request_a_character(choice)
     elif choice == '5':
         sys.exit()
-    elif choice != '4': # any choice besides those listed or return menu
+    elif choice != '4':  # any choice besides those listed or return menu
         print('Invalid choice, please choose one of the available options\n')
         extras_menu()
 
@@ -196,10 +204,10 @@ def extras_menu():
 def start():  # sourcery no-metrics
     while True:
         choice = input('(1) Search the Superhero/Villain index\n'
-                        '(2) Versus battles\n'
-                        '(3) Extras\n'
-                        '(4) Exit Program\n\n'
-                        'Which option would you like to pick: ')
+                       '(2) Versus battles\n'
+                       '(3) Extras\n'
+                       '(4) Exit Program\n\n'
+                       'Which option would you like to pick: ')
         print()
         if choice == '1':
             choice = str(input('Do you want look at the Superhero/Villain list? '))
@@ -210,7 +218,7 @@ def start():  # sourcery no-metrics
                 for x in listOfHero:
                     print(x.capitalize(), end=', ')
                 print('\n')
-            elif choice.lower() not in ['n', 'no', 'nope']: 
+            elif choice.lower() not in ['n', 'no', 'nope']:
                 # handle improper input -- anything besides 'no'
                 error_message()
             choice = str(input('Enter a Superhero/Villain name: '))
@@ -234,13 +242,14 @@ def start():  # sourcery no-metrics
             extras_menu()
         elif choice == '4':
             sys.exit()
-        else: # any choice besides those listed
+        else:  # any choice besides those listed
             print('Invalid choice, please choose an option\n')
 
 
 # to notify user of an error they made
 def user_error(message):
     print(colors.red + message, colors.reset)
+
 
 def error_message():
     print(colors.red + 'Error found!\n', colors.reset)
